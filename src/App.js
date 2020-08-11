2edacb5bb443cef03ad6997d52df34962f149e64
thomasfregene <fregzythomas@gmail.com>
2020-08-11 03:44:18 +0100

    React Hook 1 -useState contd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React,{useState} from 'react';
 
 
-const App =(initialCount)=> {
+const App =({initialCount})=> {
   let [count, setCount] = useState(initialCount)
+  let [posts, setPost] = useState([
+    {
+      name: 'Super awesome hooks',
+      body: 'Every thing is awesome when you are part of a team'
+
+    }
+  ])
 
   const addOne=()=>{
     setCount(prevCount => {
@@ -15,12 +22,35 @@ const App =(initialCount)=> {
       return prevCount - 1
     })
   }
+
+  const addOnePost=()=>{
+    let newPost ={
+      name: 'Super awesome hooks 2',
+      body: 'Every thing is awesome when you are part of a team 2'
+
+    }
+
+    setPost([
+      ...posts,
+      newPost
+    ])
+  }
    
   return (
     <>
       <h3>Count: {count}</h3>
       <button onClick={addOne}>Add one +</button>
       <button onClick={restOne}>Rest one -</button>
+      <button onClick={()=>setCount (initialCount)}>Reset</button>
+      
+      {posts.map((item, i)=>(
+        <div key={i}>
+          <hr/>
+          <div>Name: {item.name}</div>
+          <div>Body: {item.body}</div>
+        </div>
+      ))}
+      <button onClick={addOnePost}>Add one more post</button>
     </>
   );
 }
