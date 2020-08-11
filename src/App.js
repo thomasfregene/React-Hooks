f319d27678159fa79a4d05e691f61adced2497df
thomasfregene <fregzythomas@gmail.com>
2020-08-11 03:03:10 +0100

    React Hook 1 -useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React,{useState} from 'react';
 
 
-const App =()=> {
+const App =(initialCount)=> {
+  let [count, setCount] = useState(initialCount)
+
+  const addOne=()=>{
+    setCount(prevCount => {
+      return prevCount + 1
+    })
+  }
+
+  const restOne=()=>{
+    setCount(prevCount => {
+      return prevCount - 1
+    })
+  }
+   
   return (
     <>
-      <h3>Count: 0</h3>
+      <h3>Count: {count}</h3>
+      <button onClick={addOne}>Add one +</button>
+      <button onClick={restOne}>Rest one -</button>
     </>
   );
 }
